7b4768f4a62b599b6ec7f990baa296cbca84242b
Brian Vaughn <bvaughn@fb.com>
2020-01-21 14:44:29 -0800

    Checked in initial bot implementation.
    Manual testing seems good. Unit tests are failing for reasons I can't figure out yet.

--- index.js
+++ index.js
@@ -3,17 +3,104 @@
  * @param {import('probot').Application} app
  */
 module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
-
   app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
-  })
+    let { assignees, body, labels, user } = context.payload.issue;
+
+    // Convert newlines to match our local template string.
+    body = body.replace(/[\r\n]+/g, '\n');
+
+    // Check for a bug report without any repro steps.
+    // Detecting this is tricky, but an empty ordered list is a good indicator.
+    if (body.indexOf(EMPTY_ORDERED_LIST) >= 0) {
+      context.github.issues.createComment(
+        context.issue({
+          body: COMMENT_ASK_FOR_REPRO.replace('@user', `@${user.login}`),
+        })
+      );
+      if (!labels.find(label => label.name === LABEL_DEVELOPER_TOOLS)) {
+        context.github.issues.addLabels(
+          context.issue({
+            labels: [LABEL_DEVELOPER_TOOLS],
+          })
+        );
+      }
+      if (!labels.find(label => label.name === LABEL_NEEDS_INFO)) {
+        context.github.issues.addLabels(
+          context.issue({
+            labels: [LABEL_NEEDS_INFO],
+          })
+        );
+      }
+      if (!assignees.find(assignee => assignee.login === user.login)) {
+        context.github.issues.addAssignees(
+          context.issue({
+            assignees: [user.login],
+          })
+        );
+      }
+    }
+  });
+
+  app.on('issues.edited', async context => {
+    let { assignees, body, labels, user } = context.payload.issue;
+
+    // Convert newlines to match our local template string.
+    body = body.replace(/[\r\n]+/g, '\n');
+
+    // Check for a bug report without any repro steps.
+    // Detecting this is tricky, but an empty ordered list is a good indicator.
+    if (body.indexOf(EMPTY_ORDERED_LIST) < 0) {
+      // If there are repro instructions, were they added with this edit?
+      const { body: prevBody } = context.payload.changes;
+
+      if (prevBody) {
+        // Convert newlines to match our local template string.
+        prevBody.from = prevBody.from.replace(/[\r\n]+/g, '\n');
+
+        if (prevBody.from && prevBody.from.indexOf(EMPTY_ORDERED_LIST) >= 0) {
+          if (labels.find(label => label.name === LABEL_NEEDS_INFO)) {
+            context.github.issues.removeLabel(
+              context.issue({
+                name: LABEL_NEEDS_INFO,
+              })
+            );
+          }
+          if (!labels.find(label => label.name === LABEL_STATUS_UNCONFIRMED)) {
+            context.github.issues.addLabels(
+              context.issue({
+                labels: [LABEL_STATUS_UNCONFIRMED],
+              })
+            );
+          }
+          if (assignees.find(assignee => assignee.login === user.login)) {
+            context.github.issues.removeAssignees(
+              context.issue({
+                assignees: [user.login],
+              })
+            );
+          }
+        }
+      }
+    } else {
+      context.log('still there!');
+    }
+  });
 
   // For more information on building apps:
   // https://probot.github.io/docs/
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
-}
+};
+
+const LABEL_DEVELOPER_TOOLS = 'Component: Developer Tools';
+const LABEL_NEEDS_INFO = 'Resolution: Needs More Information';
+const LABEL_STATUS_UNCONFIRMED = 'Status: Unconfirmed';
+
+const EMPTY_ORDERED_LIST = `Describe what you were doing when the bug occurred:\n1. \n2. \n3. `;
+
+const COMMENT_ASK_FOR_REPRO = `@user It doesn't look like this bug report has enough info for one of us to reproduce it. Please update the issue description to add more information!
+
+Issues with no repro info may be closed.
+
+Thank you!`;
--- test/index.test.js
+++ test/index.test.js
@@ -1,55 +1,95 @@
-const nock = require('nock')
+const nock = require('nock');
+const fs = require('fs');
+const path = require('path');
+
 // Requiring our app implementation
-const myProbotApp = require('..')
-const { Probot } = require('probot')
-// Requiring our fixtures
-const payload = require('./fixtures/issues.opened')
-const issueCreatedBody = { body: 'Thanks for opening this issue!' }
-const fs = require('fs')
-const path = require('path')
+const myProbotApp = require('..');
+const { Probot } = require('probot');
 
 describe('My Probot app', () => {
-  let probot
-  let mockCert
+  let probot;
+  let mockCert;
 
-  beforeAll((done) => {
-    fs.readFile(path.join(__dirname, 'fixtures/mock-cert.pem'), (err, cert) => {
-      if (err) return done(err)
-      mockCert = cert
-      done()
-    })
-  })
+  beforeAll(done => {
+    fs.readFile(
+      path.join(__dirname, 'fixtures/mock-cert.pem'),
+      (error, cert) => {
+        if (error) return done(error);
+        mockCert = cert;
+        done();
+      }
+    );
+  });
 
   beforeEach(() => {
-    nock.disableNetConnect()
-    probot = new Probot({ id: 123, cert: mockCert })
-    // Load our app into probot
-    probot.load(myProbotApp)
-  })
+    nock.disableNetConnect();
 
-  test('creates a comment when an issue is opened', async () => {
+    probot = new Probot({ id: 123, cert: mockCert });
+    probot.load(myProbotApp);
+
     // Test that we correctly return a test token
-    nock('https://api.github.com')
-      .post('/app/installations/2/access_tokens')
-      .reply(200, { token: 'test' })
+    nock('https://api.github.com:443', { encodedQueryParams: true })
+      .post('/app/installations/6332821/access_tokens', {})
+      .reply(200, { token: 'test' });
 
-    // Test that a comment is posted
-    nock('https://api.github.com')
-      .post('/repos/hiimbex/testing-things/issues/1/comments', (body) => {
-        expect(body).toMatchObject(issueCreatedBody)
-        return true
-      })
-      .reply(200)
-
-    // Receive a webhook event
-    await probot.receive({ name: 'issues', payload })
-  })
+    // DEBUG
+    //nock.recorder.rec();
+  });
 
   afterEach(() => {
-    nock.cleanAll()
-    nock.enableNetConnect()
-  })
-})
+    nock.cleanAll();
+    nock.enableNetConnect();
+  });
+
+  test('triages issues that are opened without repro info', async done => {
+    let didReceiveComment = false;
+
+    // Test that a comment is posted
+    nock('https://api.github.com:443')
+      //.post('/repos/bvaughn/react-devtools-triage-bot/issues/1/comments', body => {
+      .post('/repos', body => {
+        didReceiveComment = true;
+        expect(body).toMatchObject(issueCreatedBody);
+        return true;
+      })
+      .reply(200);
+
+    // Receive a webhook event
+    await probot.receive({
+      name: 'issues',
+      payload: require('./fixtures/issues.opened.missing-info.json'),
+    });
+
+    expect(didReceiveComment).toBe(true);
+
+    done();
+  });
+
+  test('does not modify issues that have repro info', async done => {
+    let didReceiveComment = false;
+
+    // Test that a comment is posted
+    nock('https://api.github.com:443')
+      .post(
+        '/repos/bvaughn/react-devtools-triage-bot/issues/1/comments',
+        body => {
+          didReceiveComment = true;
+          return true;
+        }
+      )
+      .reply(200);
+
+    // Receive a webhook event
+    await probot.receive({
+      name: 'issues',
+      payload: require('./fixtures/issues.opened.has-info.json'),
+    });
+
+    expect(didReceiveComment).toBe(false);
+
+    done();
+  });
+});
 
 // For more information about testing with Jest see:
 // https://facebook.github.io/jest/
